e119ce9f03a02a46e5fa866e2f472c5547116a54
David Novák <kaladivo@gmail.com>
2020-11-29 20:44:56 +0100

    Add migration script into entrypoint.sh directly

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-yarn migrate
+# migrate db
+DATABASE_URL="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_DATABASE" \
+  yarn node-pg-migrate $1 --migrations-schema migrations --create-migrations-schema --tsconfig ./tsconfig.server.json -j ts
 
 exec "$@"
